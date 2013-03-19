e503da1ecd97abad7cb043e53c63ea4e5725627c
Michael Lawrence <michael.lawrence@rhythmagency.com>
2013-03-19 16:54:32 -0700

    Updating to match jquery.pluginTemplate updates.

--- jquery.reverse.js
+++ jquery.reverse.js
@@ -12,7 +12,7 @@
 
 		exports[plugin.name] = plugin.fn;
 	} else if (typeof define === 'function' && define.amd) { // AMD
-		define(['jquery'], function ($) {
+		define(plugin.deps, function ($) {
 			var fn = (plugin.isStatic ? $ : $.fn)[plugin.name] = plugin.factory($);
 			return fn;
 		});
@@ -27,6 +27,7 @@
 })(window.$, {
 	'name' : 'reverse',
 	'isStatic' : false,
+	'deps' : ['jquery'],
 	'factory' : function ($) {
 		return function () {
 			return [].reverse;
