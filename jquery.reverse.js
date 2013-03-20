c1ecf970b93269eb3bd036cab755f607aeba2f9a
Michael Lawrence <michael.lawrence@rhythmagency.com>
2013-03-20 08:17:20 -0700

    Removing plugin.deps as it doesn't make sense since you have to map the dependencies to variables.

--- jquery.reverse.js
+++ jquery.reverse.js
@@ -12,7 +12,7 @@
 
 		exports[plugin.name] = plugin.fn;
 	} else if (typeof define === 'function' && define.amd) { // AMD
-		define(plugin.deps, function ($) {
+		define(['jquery'], function ($) {
 			var fn = (plugin.isStatic ? $ : $.fn)[plugin.name] = plugin.factory($);
 			return fn;
 		});
@@ -27,7 +27,6 @@
 })(window.$, {
 	'name' : 'reverse',
 	'isStatic' : false,
-	'deps' : ['jquery'],
 	'factory' : function ($) {
 		return function () {
 			return [].reverse;
